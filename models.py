3c6f41d53bccbc8f0a0957767c7080b0a6c9ab2d
akashpoojari1450-crypto <akashpoojari1450@gmail.com>
2026-04-07 21:44:47 +0530

    Update models.py

--- models.py
+++ models.py
@@ -4,10 +4,10 @@ from typing import Optional, Dict
 class NewsObservation(BaseModel):
     echoed_message: str
     message_length: int
-    done: bool
+    done: bool = False
     reward: Optional[float] = None
     metadata: Dict = {}
 
 class DetectionAction(BaseModel):
     message: str
-    label: Optional[str] = None # Added label for the Environment class
+    label: Optional[str] = "FAKE"
