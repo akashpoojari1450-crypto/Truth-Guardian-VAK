37280262a1b7f48af4ddba3e5e1dbfbabc5ff16d
akashpoojari1450-crypto <akashpoojari1450@gmail.com>
2026-04-08 10:17:01 +0530

    Update bank_cloud.py

--- server/bank_cloud.py
+++ server/bank_cloud.py
@@ -1,21 +1,21 @@
 import random
 import time
 
-def verify_with_bank_hq(otp_code):
+def verify_with_bank_hq(top_code):
     """
     Simulates the Bank's Backend Database.
-    In a real system, this checks if the Bank's server 
+    In a real system, this checks if the Bank's server
     generated this OTP for Akash's phone number.
     """
-    print(f"🔱 [BANK-HQ] Verifying Session for OTP: {otp_code}...")
+    print(f"【BANK-HQ】 Verifying Session for OTP: {top_code}...")
     
     # Simulate network latency (The "Bank Delay")
-    time.sleep(2) 
-    
-    # 🔱 THE TRUTH LOGIC:
+    time.sleep(2)
+
+    # THE TRUTH LOGIC:
     # We simulate a 20% chance it's a real bank session
     # and an 80% chance it's a fraudulent intercept.
-    is_official_session = random.random() < 0.2 
+    is_official_session = random.random() < 0.2
     
     if is_official_session:
         return "SUCCESS: OFFICIAL_BANK_SESSION_CONFIRMED"
